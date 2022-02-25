5ab4947d66279c6d688616907b8f8e1b6b2a02b6
Toni Guijarro <aguijarror@esliceu.net>
2022-02-25 19:40:49 +0100

    ps alfinal si

--- index.js
+++ index.js
@@ -19,5 +19,16 @@ function calculaIVA(){
 }
 
 function canviaFons(tipus){
+    if (tipus == "random"){
+        var red = Math.floor(Math.random() * 255);
+        var green = red;
+        var blue = red;
+        FondoAleatorio = "rgb(" + red + "," + green + "," + blue + ")";
+}  else if(tipus== "clear"){
+        FondoAleatorio="white";
+}
+    var FondoAleatorio;
+    _id("cos").style.backgroundColor = FondoAleatorio;
+}
 
-}  
+
